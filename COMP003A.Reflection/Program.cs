1e0546ba7be7668d79714d9a8a5acf11e6a9099d
Michael Baker <michaelbaker4@my.whccd.edu>
2025-03-01 00:00:09 -0800

    Answers

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -8,13 +8,13 @@ namespace COMP003A.Reflection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What worked well for you over the last 8 weeks?");
-            Console.WriteLine("What external resources did you use to aid your learning?");
-            Console.WriteLine("What would you change about the course?");
-            Console.WriteLine("Did you learn what you expected to learn in this course?");
-            Console.WriteLine("What concepts were new to you?");
-            Console.WriteLine("What concepts have you encountered already in the past?");
-            Console.WriteLine("Do you have any tips/comments for the instructor?");
+            Console.WriteLine("What worked well for you over the last 8 weeks? \n Working from home worked well-ish for me, although being able to attend more lectures definitely would have helped deepen my understanding.");
+            Console.WriteLine("What external resources did you use to aid your learning? \n I used mostly stack overflow. Bing AI did help out but only as far as helping when it came to finding a pathway towards a solution. Stack Overflow was the main portion of where I got my deeper learning done.");
+            Console.WriteLine("What would you change about the course? \n There's not much I would change about the course, it would be nice for a full semester class like this just to have a little more time when it came to assignments. Some of the module assignments that opened Wednesday at 12:00am and due same day by 11:59pm seemed impossible for people who had other classes and other responsibilities.");
+            Console.WriteLine("Did you learn what you expected to learn in this course? \n I learned a lot more than I expected in this course.");
+            Console.WriteLine("What concepts were new to you? \n Most of everything past module 3 was new to me. Lists and arrays were new and interesting, learning about OOP was challenging and interesting, learning how methods worked in a program proved a nice challenge as well.");
+            Console.WriteLine("What concepts have you encountered already in the past? \n A lot of the beginning concepts that were covered are things that I learned either on my own or in previous coding classes. How to make a comment, boolean expressions, logic gates, how to output a line of code to a screen.");
+            Console.WriteLine("Do you have any tips/comments for the instructor? \n Just 1 tip and 1 comment: Tip: please give more time for the bigger assignments towards the end of the 8 weeks. Modules 5 and 6 were brutal to get through even with multiple days to get them done. \n Comment: You were an excellent instructor who was more than willing to help me succeed in any way you could and I'm glad we were able to communicate effectively.");
 
         }
     }
